1e9f09e2b37296241f5fe4d5ecd8d57c16655f0c
matejabogdanovic <bogdanovicmateja75@gmail.com>
2025-08-17 10:28:40 -0700

    Better example.

--- input.s
+++ input.s
@@ -1,18 +1,20 @@
 .section a
-#push %r2
-#pop %r4
-#call b
-#call 0x0f
+# push %r2
+# pop %r4
+# call b
+# call 0x0f
+
+ jmp b
+ jmp 0x69
 .skip 1
 b:
- 
- jmp 0x0f
+
  .skip 2
  
  push %r2
 
-#iret
-#ret
+# iret
+# ret
 .section b
 jmp b
 .skip 1
