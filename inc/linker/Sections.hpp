81139694e434138089ed2b6505e4d880c3dbad87
matejabogdanovic <bogdanovicmateja75@gmail.com>
2025-08-25 01:40:42 -0700

    Added order.

--- inc/linker/Sections.hpp
+++ inc/linker/Sections.hpp
@@ -7,21 +7,29 @@ public:
     FileState* file;
     SymbolTable::Entry* section;
   };
-
+  int globalOrder = 0;
   struct SectionsUnion{
+    int order;
     std::string name = ""; 
     uint32_t start_address = 0;
     bool fixed = false;
     uint64_t size = 0;
     std::vector <Section> sections; // aggregate    
+
+    
   };
   struct SortedByStartAddress {
     bool operator()(const SectionsUnion& a, const SectionsUnion& b) const {
-        return a.start_address < b.start_address;
+      if(a.start_address == b.start_address)
+        return a.order < b.order;
+      return a.start_address < b.start_address;
     }
   };
-  std::set <SectionsUnion, SortedByStartAddress> map;
+  std::multiset <SectionsUnion, SortedByStartAddress> map;
 
   // returns true if new section union is made
   bool put(FileState* file,std::string* section_name, SymbolTable::Entry* section,  uint32_t* start_address=nullptr);
+
+private:
+void reorderSections();
 };
